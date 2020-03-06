50a00e7b24d0670e87dc8aaaf6fa33766d180b7f
Amduras <sip90@web.de>
2020-03-06 17:05:05 +0100

    nutzerverwaltung done

--- src/main/java/controller/RoleHandler.java
+++ src/main/java/controller/RoleHandler.java
@@ -5,9 +5,21 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.annotation.Resource;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
 
+import enums.AuthLvl;
 import model.User;
 
 @ManagedBean(name="roles")
@@ -19,14 +31,15 @@ public class RoleHandler implements Serializable{
 	private User selectedUser;
 
 	private List<String> rights = new ArrayList<>( Arrays.asList("BANNED", "RESTRICTED", "USER", "GA", "SGA"));
-	private String newRight; 
+	private String newRight;
+	
+	@PersistenceContext
+	private EntityManager em;
+
+	@Resource
+	private UserTransaction utx;
 	
 	public RoleHandler() {
-	}
-	
-	public void onRowSelect() {
-		System.out.println("Test");
-		System.out.println("User: "+selectedUser.getUsername());
 	}
 	
 	public User getUser() {
@@ -50,7 +63,7 @@ public class RoleHandler implements Serializable{
 	}
 
 	public void setSelectedUser(User selectedUser) {
-		System.out.println("Gesetzt:" +selectedUser);
+		System.out.println("Gesetzt:" +selectedUser.getUsername());
 		this.selectedUser = selectedUser;
 	}
 
@@ -59,6 +72,33 @@ public class RoleHandler implements Serializable{
 	}
 
 	public void setNewRight(String newRight) {
-		this.newRight = newRight;
+		if(selectedUser != null) {
+			if(!newRight.toLowerCase().equals(selectedUser.getAuthLvl().getLabel())) {
+				this.newRight = newRight;
+				Query query = em.createQuery("update User set authlvl = :authlvl where username = :username");
+				query.setParameter("authlvl", (int) AuthLvl.valueOf(newRight).ordinal());
+				query.setParameter("username", selectedUser.getUsername());
+				try {
+					utx.begin();
+				} catch (NotSupportedException | SystemException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				query.executeUpdate();
+				try {
+					utx.commit();
+				} catch (SecurityException | IllegalStateException | RollbackException | HeuristicMixedException
+						| HeuristicRollbackException | SystemException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				FacesContext.getCurrentInstance().getViewRoot().getViewMap().remove("roles");
+			} else {
+				System.out.println("Nimm ne andere rolle trottel!");
+			}
+		} else {
+			System.out.println("Kein User gewählt");
+		}
+		
 	}
 }
