8b0c80c12e080ff62d84fcf313e48233d408b7d0
Amduras <sip90@web.de>
2020-04-11 15:20:43 +0200

    no pictures4Ugaming

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -31,7 +31,6 @@ public class User implements Serializable{
 	private String password;
 	private IsActive isActive;
 	private Long lastlogin;
-	private int picture;
 	private AuthLvl authLvl;
 	
 	public User(){
